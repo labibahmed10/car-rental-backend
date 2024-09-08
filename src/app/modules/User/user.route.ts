35cfd5e6a766b90879febcbff96c9e2b31f7741d
labibahmed10 <labib.ahmed.372@gmail.com>
2024-09-08 16:41:44 +0600

    fix: fixed route for signup, added validation

--- src/app/modules/User/user.route.ts
+++ src/app/modules/User/user.route.ts
@@ -1,9 +1,11 @@
 import express from "express";
 import { UserController } from "./user.controller";
+import validateRequest from "../../middleware/validateRequest";
+import { userValidationSchema } from "./user.validation";
 
 const router = express.Router();
 
 // signup route
-router.post("/signup", UserController.signUpUser);
+router.post("/signup", validateRequest(userValidationSchema.createUserValidationSchema), UserController.signUpUser);
 
 export const UserRoutes = router;
--- src/app/modules/User/user.services.ts
+++ src/app/modules/User/user.services.ts
@@ -1,11 +1,18 @@
+import httpStatus from "http-status";
+import AppError from "../../error/AppError";
 import { IUser } from "./user.interface";
 import { UserModel } from "./user.model";
 
 // user signup
 const signUpUserIntoDB = async (payload: IUser) => {
-  const createdData = await UserModel.create(payload);
-  return createdData;
-};
+  const emailExist = await UserModel.findOne({ email: payload.email });
+  if (emailExist) {
+    throw new AppError(httpStatus.BAD_REQUEST, "User already exist");
+  }
+
+  const result = await UserModel.create(payload);
+  return result;
+};  
 
 export const UserServices = {
   signUpUserIntoDB,
