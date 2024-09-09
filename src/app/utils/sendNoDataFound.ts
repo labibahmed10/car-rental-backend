17edfb9ed7e0434235e01a3a18a3c44c3509c436
labibahmed10 <labib.ahmed.372@gmail.com>
2024-09-10 02:03:44 +0600

    fix: http status code changed, no route found added

--- src/app/utils/sendNoDataFound.ts
+++ src/app/utils/sendNoDataFound.ts
@@ -1,7 +1,8 @@
 import { Response } from "express";
+import httpStatus from "http-status";
 
 const sendNoDataFound = (res: Response) => {
-  return res.status(404).json({
+  return res.status(httpStatus.NOT_FOUND).json({
     success: false,
     statusCode: 404,
     message: "No Data Found",
